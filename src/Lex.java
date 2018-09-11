c86243458a30e24ee7c9cf5d35e7f91f46f1048f
Marco Robles <mrobles@wiggot.com>
2018-09-10 23:19:34 -0500

    setup project code for work

---
+++ src/Lex.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lex {
+
+    private static final HashMap<String, Integer> reserved_words = new HashMap<>();
+    private static final HashMap<String, Integer> tokens = new HashMap<>();
+    private int tokenPosition;
+
+    public Lex() {
+
+    }
+}
